660883d763bb914bb0805b085910019b55b9ee81
V MukeshSanghani <v.mukeshsanghani@perficient.com>
2020-06-26 16:35:27 +0530

    Hosting in Netlify

---
+++ src/components/Loader.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import "bootstrap/dist/css/bootstrap.css";
+import * as legoData from "../lottiefiles.json";
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: legoData.default,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    }
+}
+
+export default class Loading extends React.Component {
+    render() {
+        return (
+            <div>
+                <FadeIn>
+                    <div class="d-flex justify-content-center align-items-center">
+                        {/* <h1>fetching pizza</h1> */}
+                        <div style = {{"marginTop" : 200}}>
+                            <Lottie options={defaultOptions} height={200} width={200} />
+                        </div>
+                    </div>
+                </FadeIn>
+            </div>
+        )
+    }
+}
--- src/components/mainView.js
+++ src/components/mainView.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 import moment from 'moment-timezone';
 
-import { internal_data } from './internal_data';
+// import { internal_data } from './internal_data';
 import Heading from './Heading';
 import Countdown from './countdown';
+import Loading from './Loader';
 
 import { Divider, Grid, Image, Segment } from 'semantic-ui-react';
 
@@ -25,59 +26,60 @@ class MainView extends React.Component {
     }
 
     async logging() {
-        const short = internal_data;
-        // console.log(data);
+        // const short = internal_data;
+        // // console.log(data);
 
-        // const short = data.data.api.fixtures;
-        for (let i = 415; i < short.length; i++) {
-            const m_date = short[i].event_date.split('T')[0];
-            if (moment(m_date).isSameOrAfter(moment().format("YYYY-MM-DD"))) {
-                console.log(short[i]);
-
-                this.setState({
-                    current_fixture: short[i],
-                    home_Coach: "Home Coach",
-                    away_Coach: "Away Coach"
-                });
-                return;
-            }
-        }
-        return;
-
-
-        // const data = await axios.get("https://api-football-v1.p.rapidapi.com/v2/fixtures/team/33", {
-        //     "method": "GET",
-        //     "headers": {
-        //         "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-        //         "x-rapidapi-key": "87b140eb05msh461aa1d79520e3ep12982ejsnbdaf7d3dac51"
-        //     }
-        // })
-        // const short = data.data.api.fixtures;
+        // // const short = data.data.api.fixtures;
         // for (let i = 415; i < short.length; i++) {
         //     const m_date = short[i].event_date.split('T')[0];
-        //     // console.log((moment().format("YYYY-MM-DD")).isSameOrBefore(moment(m_date)));
-
         //     if (moment(m_date).isSameOrAfter(moment().format("YYYY-MM-DD"))) {
         //         console.log(short[i]);
-        // const home_Coach = await axios.get(`https://api-football-v1.p.rapidapi.com/v2/coachs/team/${short[i].homeTeam.team_id}`, {
-        //             "method": "GET",
-        //             "headers": {
-        //                 "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-        //                 "x-rapidapi-key": "87b140eb05msh461aa1d79520e3ep12982ejsnbdaf7d3dac51"
-        //             }
-        //         })
-        //         const away_Coach = await axios.get(`https://api-football-v1.p.rapidapi.com/v2/coachs/team/${short[i].awayTeam.team_id}`, {
-        //             "method": "GET",
-        //             "headers": {
-        //                 "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-        //                 "x-rapidapi-key": "87b140eb05msh461aa1d79520e3ep12982ejsnbdaf7d3dac51"
-        //             }
-        //         })
-        //         this.setState({ current_fixture: short[i] });
+
+        //         this.setState({
+        //             current_fixture: short[i],
+        //             home_Coach: "Home Coach",
+        //             away_Coach: "Away Coach"
+        //         });
         //         return;
         //     }
         // }
         // return;
+
+
+        const data = await axios.get("https://api-football-v1.p.rapidapi.com/v2/fixtures/team/33", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+                "x-rapidapi-key": "87b140eb05msh461aa1d79520e3ep12982ejsnbdaf7d3dac51"
+            }
+        })
+        const short = data.data.api.fixtures;
+        for (let i = 415; i < short.length; i++) {
+            const m_date = short[i].event_date.split('T')[0];
+            // console.log((moment().format("YYYY-MM-DD")).isSameOrBefore(moment(m_date)));
+
+            if (moment(m_date).isSameOrAfter(moment().format("YYYY-MM-DD"))) {
+                let home_Coach = await axios.get(`https://api-football-v1.p.rapidapi.com/v2/coachs/team/${short[i].homeTeam.team_id}`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+                        "x-rapidapi-key": "87b140eb05msh461aa1d79520e3ep12982ejsnbdaf7d3dac51"
+                    }
+                })
+                let away_Coach = await axios.get(`https://api-football-v1.p.rapidapi.com/v2/coachs/team/${short[i].awayTeam.team_id}`, {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+                        "x-rapidapi-key": "87b140eb05msh461aa1d79520e3ep12982ejsnbdaf7d3dac51"
+                    }
+                })
+                home_Coach = home_Coach.data.api.coachs[0].firstname + home_Coach.data.api.coachs[0].lastname
+                away_Coach = away_Coach.data.api.coachs[0].firstname + away_Coach.data.api.coachs[0].lastname
+                this.setState({ current_fixture: short[i], home_Coach: home_Coach, away_Coach: away_Coach });
+                return;
+            }
+        }
+        return;
     }
 
     render() {
@@ -85,50 +87,51 @@ class MainView extends React.Component {
             <div>
                 {this.state.current_fixture ?
 
-                    <div style = {{"position": "absolute"}}>
-                       
-                            <Heading league={this.state.current_fixture.league.name} />
-                            <Segment style={{ "margin": "10px 400px", "backgroundColor": "transparent" }}>
-                                <Grid columns={2} >
-                                    <Grid.Column>
-                                        <Image src={this.state.current_fixture.homeTeam.logo}
-                                            alt="Home-Team"
-                                            className="ui centered small image" />
-                                        <p className="center_it">
-                                            {this.state.current_fixture.homeTeam.team_name}
-                                        </p>
-                                        <p className="center_it">
-                                            Coach : {this.state.home_Coach}
-                                        </p>
-                                    </Grid.Column>
-                                    <Grid.Column>
-                                        <Image src={this.state.current_fixture.awayTeam.logo}
-                                            alt="Away-Team"
-                                            className="ui centered small image" />
-                                        <p className="center_it">
-                                            {this.state.current_fixture.awayTeam.team_name}
-                                        </p>
-                                        <p className="center_it">
-                                            Coach : {this.state.away_Coach}
-                                        </p>
-                                    </Grid.Column>
-                                </Grid>
+                    <div style={{ "position": "absolute" }}>
 
-                                <Divider vertical>
-                                    VS
+                        <Heading league={this.state.current_fixture.league.name} />
+                        <Segment style={{ "margin": "10px", "marginLeft": "100px", "backgroundColor": "transparent" }} size={'massive'}>
+
+                            <Grid columns={2}>
+                                <Grid.Column>
+                                    <Image src={this.state.current_fixture.homeTeam.logo}
+                                        alt="Home-Team"
+                                        className="ui centered small image" />
+                                    <p className="center_it">
+                                        {this.state.current_fixture.homeTeam.team_name}
+                                    </p>
+                                    <p className="center_it">
+                                        Coach : {this.state.home_Coach}
+                                    </p>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Image src={this.state.current_fixture.awayTeam.logo}
+                                        alt="Away-Team"
+                                        className="ui centered small image" />
+                                    <p className="center_it">
+                                        {this.state.current_fixture.awayTeam.team_name}
+                                    </p>
+                                    <p className="center_it">
+                                        Coach : {this.state.away_Coach}
+                                    </p>
+                                </Grid.Column>
+                            </Grid>
+
+                            <Divider vertical>
+                                VS
                             </Divider>
-                            </Segment>
+                        </Segment>
 
-                            <p className="center_it">
-                                Venue : {this.state.current_fixture.venue}
-                            </p>
-                            <br />
-                            <br />
-                            <Countdown timeTillDate={this.t_convert(this.state.current_fixture.event_date)} />
+                        <p className="center_it">
+                            Venue : {this.state.current_fixture.venue}
+                        </p>
+                        <br />
+                        <br />
+                        <Countdown timeTillDate={this.t_convert(this.state.current_fixture.event_date)} />
                     </div>
                     :
                     <div>
-                        "Loading..."
+                        <Loading />
                     </div>
                 }
             </div>
